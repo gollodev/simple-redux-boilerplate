6e6e6115cefb4f2114f80dc8186b4cb2323be55b
Jose Lopez <gollo_autumn@live.com>
2016-04-24 19:55:18 -0430

    Added Material-UI test

--- src/js/components/FriendListItem/FriendListItem.js
+++ src/js/components/FriendListItem/FriendListItem.js
@@ -2,6 +2,7 @@ import './FriendListItem.scss';
 
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
+import { RaisedButton } from 'material-ui';
 
 export default class FriendListItem extends Component {
 
@@ -21,12 +22,8 @@ export default class FriendListItem extends Component {
         <div><small>xx friends in common</small></div>
         </div>
         <div className="friendActions">
-          <button className="btn btn-default btnAction" onClick={() => this.props.starFriend(this.props.id)}>
-            <i className={classnames('fa', { 'fa-star': this.props.starred }, { 'fa-star-o': !this.props.starred })} />
-          </button>
-          <button className="btn btn-default btnAction" onClick={() => this.props.deleteFriend(this.props.id)}>
-            <i className="fa fa-trash" />
-          </button>
+          <RaisedButton label="Starred" secondary={true} onClick={() => this.props.starFriend(this.props.id)} />          
+          <RaisedButton label="Delete" secondary={true} onClick={() => this.props.deleteFriend(this.props.id)} />          
         </div>
       </li>
     );
--- src/js/containers/FriendListApp/FriendListApp.js
+++ src/js/containers/FriendListApp/FriendListApp.js
@@ -3,6 +3,7 @@ import './FriendListApp.scss';
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { AppBar } from 'material-ui';
 
 import * as FriendsActions from '../../actions/FriendsActions';
 import { AddFriendInput, FriendList } from '../../components';
@@ -14,9 +15,9 @@ class FriendListApp extends Component {
     console.log(this.props);
     return (      
       <div className="friendListApp">
-        <h1>Da Bro's List</h1>
+        <AppBar title="Da Bro's List"/>     
         <AddFriendInput addFriend={actions.addFriend} />
-        <FriendList friends={friendsById} actions={actions} />
+        <FriendList friends={friendsById} actions={actions} />        
       </div>
     );
   }
